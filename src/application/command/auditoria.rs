61a7db62ed662d5a7c84fb0f27dadf4591cbd686
VictorMagrdev <victorakiladev@gmail.com>
2024-11-16 11:55:07 -0500

    mongo implementacion

--- src/application/command/auditoria.rs
+++ src/application/command/auditoria.rs
@@ -1,7 +1,6 @@
 use crate::infrastructure::data::db_mongo::ClientState;
-use crate::infrastructure::data::get_db::GetDb;
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::http::{Error, StatusCode};
 use axum::response::IntoResponse;
 use axum::Json;
 use serde_json::Value;
@@ -9,21 +8,24 @@ use serde_json::Value;
 pub async fn post_auditoria(
     State(client): State<ClientState>,
     Json(payload): Json<Value>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
+) -> Result<impl IntoResponse, Error> {
     Ok(StatusCode::CREATED)
 }
-
-pub async fn delete_auditoria(
-    State(client): State<ClientState>,
-    Path(id): Path<i32>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    Ok(StatusCode::OK)
-}
-
-pub async fn put_auditoria(
-    State(client): State<ClientState>,
-    Path(id): Path<i32>,
-    Json(payload): Json<Value>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    Ok(StatusCode::OK)
-}
+ 
+ pub async fn delete_auditoria(
+     State(client): State<ClientState>,
+     Path(id): Path<i32>,
+ ) -> Result<impl IntoResponse, Error>  {
+     todo!("a futuro");
+     Ok(StatusCode::OK)
+ }
+ 
+ pub async fn put_auditoria(
+     State(client): State<ClientState>,
+     Path(id): Path<i32>,
+     Json(payload): Json<Value>,
+ ) -> Result<impl IntoResponse, Error>  {
+     todo!("a futuro");
+     Ok(StatusCode::OK)
+     
+ }
