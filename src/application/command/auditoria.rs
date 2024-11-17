404c1423277b1c4507b56b20a78c89162b1b2cdb
JuanFosoA <yioyiomain@gmail.com>
2024-11-16 23:46:50 -0500

    Añadiendo update by id

--- src/application/command/auditoria.rs
+++ src/application/command/auditoria.rs
@@ -80,14 +80,47 @@ pub async fn delete_auditoria(
         }
     }
 }
-/*
-pub async fn put_auditoria(
-    State(client): State<ClientState>,
-    Path(id): Path<i32>,
-    Json(payload): Json<Value>,
-) -> Result<impl IntoResponse, Error>  {
-    todo!("a futuro");
-    Ok(StatusCode::OK)
 
+pub async fn put_auditoria(
+    State(client): State<AppState>,
+    Path(id): Path<i64>,
+    Json(payload): Json<Value>,
+) -> Result<impl IntoResponse, StatusCode>  {
+
+    let fecha = payload.get("fecha")
+        .and_then(|v| v.as_str())
+        .and_then(|s| NaiveDate::parse_from_str(s, "%Y-%m-%d").ok())
+        .ok_or(StatusCode::BAD_REQUEST)?;
+    let nombre_paciente = payload.get("nombre_paciente")
+        .and_then(|v| v.as_str()).unwrap_or("").to_string();
+    let nombre_doctor = payload.get("nombre_doctor")
+        .and_then(|v| v.as_str()).unwrap_or("").to_string();
+    let motivo_cita = payload.get("motivo_cita")
+        .and_then(|v| v.as_str()).unwrap_or("").to_string();
+    let diagnostico = payload.get("diagnostico")
+        .and_then(|v| v.as_str()).unwrap_or("").to_string();
+    let medicamentos_recetados = payload.get("medicamentos_recetados")
+        .and_then(|v| v.as_str()).unwrap_or("").to_string();
+
+    let auditorias = client.get_db_mongo().database("doctorya")
+        .collection::<bson::Document>("auditoria");
+
+    let update = doc! {"$set": doc! {
+        "fecha": fecha.to_string(),
+        "nombre_paciente": nombre_paciente,
+        "nombre_doctor": nombre_doctor,
+        "motivo_cita": motivo_cita,
+        "diagnostico": diagnostico,
+        "medicamentos_recetados": medicamentos_recetados}};
+
+    match auditorias.update_one(doc! { "id": id }, update ).await{
+        Ok(result) => {
+            println!("Documento eliminado: {:?}", result.modified_count);
+            Ok(StatusCode::OK)
+        }
+        Err(err) => {
+            eprintln!("Error al insertar el documento: {:?}", err);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
-*/
