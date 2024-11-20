4469be3835a48f16390235373a0f047707282603
JuanFosoA <yioyiomain@gmail.com>
2024-11-20 17:42:24 -0500

    Añadiendo funcionalidades de calendario

---
+++ fns_calendario.sql
@@ -0,0 +1,38 @@
+--public.calendario
+
+create or replace function public.calendario_por_medico(
+	p_medico_id int
+) returns table (
+	v_nombre_medico varchar,
+	v_fecha date,
+	v_hora time,
+	v_nombre_paciente varchar
+) as $$
+begin
+	return query
+	select m.nombre, c.fecha, c.hora, p.nombre from public.citas c
+	inner join public.medicos m on c.medico_id = m.id
+	inner join public.pacientes p on c.paciente_id = p.id
+	where c.medico_id = p_medico_id
+	and c.estado = 'programada';
+end;
+$$ language plpgsql;
+
+
+create or replace function public.calendario_por_especialidad(
+	p_especialidad_id int
+) returns table (
+	v_nombre_medico varchar,
+	v_fecha date,
+	v_hora time,
+	v_nombre_paciente varchar
+) as $$
+begin
+	return query
+	select m.nombre, c.fecha, c.hora, p.nombre from public.citas c
+	inner join public.medicos m on c.medico_id = m.id
+	inner join public.pacientes p on c.paciente_id = p.id
+	where m.especialidad_id = p_especialidad_id
+		and c.estado = 'programada';
+end;
+$$ language plpgsql;
