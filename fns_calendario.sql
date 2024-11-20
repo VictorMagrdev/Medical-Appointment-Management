fd67ea1b37826bbb95bdb373b143386a626d7944
JuanFosoA <yioyiomain@gmail.com>
2024-11-20 18:04:18 -0500

    Listar citas de un paciente

--- fns_calendario.sql
+++ fns_calendario.sql
@@ -36,3 +36,23 @@ begin
 		and c.estado = 'programada';
 end;
 $$ language plpgsql;
+
+
+create or replace function public.calendario_por_paciente(
+	p_paciente_id int
+) returns table (
+	v_nombre_paciente varchar,
+	v_fecha date,
+	v_hora time,
+	v_nombre_medico varchar
+) as $$
+begin
+	return query
+	select p.nombre, c.fecha, c.hora, m.nombre from public.citas c
+	inner join public.medicos m on c.medico_id = m.id
+	inner join public.pacientes p on c.paciente_id = p.id
+	where m.especialidad_id = p_especialidad_id;
+end;
+$$ language plpgsql;
+
+
